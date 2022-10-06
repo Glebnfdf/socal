ef00be4860285727f7304d25c0f32e17ac04fba8
kotix848 <katocik@protonmail.com>
2022-10-07 00:06:15 +0300

    прогнал scss через линтер, удалил шрифты которые не используются.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-  plugins: ['stylelint-prettier', 'postcss-scss'],
   extends: ['stylelint-prettier/recommended'],
-  customSyntax: "postcss-scss",
-  rules: {
-    'prettier/prettier': [
-      true,
-      {
-        singleQuote: false,
-        tabWidth: 2,
-      },
-    ],
-    'declaration-no-important': true,
-    'length-zero-no-unit': true,
-    'selector-class-pattern':
-      '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
-  },
+  customSyntax: "postcss-scss"
 };
