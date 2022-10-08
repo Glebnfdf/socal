fea4022bb7099936567e9694ccb176ebab2a24ce
Milyutin Sergey <mcm@mail.ru>
2022-10-08 19:31:53 +0300

    feat(auth): если поле ввода номера телефона пустое, то кнопка Next получает особый стиль

--- src/components/Authorization/AuthScreenMng/InsertPhone/InsertPhone.tsx
+++ src/components/Authorization/AuthScreenMng/InsertPhone/InsertPhone.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 import { AuthScreenName } from "../AuthScreenMng";
 import "./insertPhone.scss";
+import { useState } from "react";
 
 interface iProps {
   changeScreen: (newScreen: AuthScreenName) => void
 }
 
 export default function InsertPhone({changeScreen}: iProps): JSX.Element {
+  const [phoneNumber, setPhoneNumber]: [st: string, set: (st: string) => void] = useState("");
+
   return (
     <div className={"insert-phone-cont"}>
       <div>
-        <input type={"tel"}/>
+        <input
+          type={"tel"}
+          value={phoneNumber}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {setPhoneNumber(event.target.value)}}
+        />
         <div>There is no user with phone +1 403 905 88 78</div>
-        <button onClick={(): void => {changeScreen(AuthScreenName.InsertSMS)}}>Next</button>
+        <button
+          className={phoneNumber.length === 0 ? "next-btn-disable" : ""}
+          onClick={(): void => {changeScreen(AuthScreenName.InsertSMS)}}
+        >Next</button>
       </div>
     </div>
   );
