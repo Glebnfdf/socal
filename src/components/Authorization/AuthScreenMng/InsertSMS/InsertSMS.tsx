7e1e464889f5ce77a502be8b75099a134612d729
Milyutin Sergey <mcm@mail.ru>
2022-10-09 22:38:16 +0300

    feat(auth): базовая вёрстка для экрана ввода смс кода

--- src/components/Authorization/AuthScreenMng/InsertSMS/InsertSMS.tsx
+++ src/components/Authorization/AuthScreenMng/InsertSMS/InsertSMS.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { AuthScreenName } from "../AuthScreenMng";
+import Preloader from "../../../Preloader/Preloader";
 
 interface iProps {
   changeScreen: (newScreen: AuthScreenName) => void
@@ -8,7 +9,58 @@ interface iProps {
 export default function InsertSMS({changeScreen}: iProps): JSX.Element {
   return (
     <>
-      <button onClick={(): void => {changeScreen(AuthScreenName.InsertPhone)}}>Back</button>
+      <div className={"insert-phone-cont"}>
+        <div>We send sms to + 1 (234) 567 -89 - 00</div>
+        <div>
+          <input
+            type={"number"}
+            // value={phoneNumber}
+            // className={"error-border" + (showErr ? " active" : "")}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+              // setShowErr(false);
+              // setPhoneNumber(event.target.value)
+            }}
+          />
+          <input
+            type={"number"}
+            // value={phoneNumber}
+            // className={"error-border" + (showErr ? " active" : "")}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+              // setShowErr(false);
+              // setPhoneNumber(event.target.value)
+            }}
+          />
+          <input
+            type={"number"}
+            // value={phoneNumber}
+            // className={"error-border" + (showErr ? " active" : "")}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+              // setShowErr(false);
+              // setPhoneNumber(event.target.value)
+            }}
+          />
+          <input
+            type={"number"}
+            // value={phoneNumber}
+            // className={"error-border" + (showErr ? " active" : "")}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+              // setShowErr(false);
+              // setPhoneNumber(event.target.value)
+            }}
+          />
+          {/*{isLoading && <Preloader/>}*/}
+          {/*{showErr && <div>Wrong code</div>}*/}
+          <div>
+            <button
+              // className={phoneNumber.length === 0 || showErr ? "next-btn-disable" : ""}
+              // onClick={(): void => {sendPhone2Server()}}
+            >Resend Code</button>
+          </div>
+          <div>
+            <button onClick={(): void => {changeScreen(AuthScreenName.InsertPhone)}}>Back</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
