d04495329e9bb307eed799685f3dd219050d8c0d
Milyutin Sergey <mcm@mail.ru>
2022-10-12 19:17:15 +0300

    feat(gant/head): интеграция вёрстки шапки

--- src/components/GantPage/Header/Header.tsx
+++ src/components/GantPage/Header/Header.tsx
@@ -1,7 +1,41 @@
 import * as React from "react";
+import "../../../../source/img/svgIcons/arrow-left.svg";
+import "../../../../source/img/svgIcons/arrow-right.svg";
+import "../../../../source/img/svgIcons/calendar-icon.svg";
+import imgLogo from "../../../../source/img/logo/logo.svg";
 
 export default function Header(): JSX.Element {
   return (
-    <></>
+    <header className="header">
+      <nav className="nav container">
+        <div className="left">
+          <div className="logo">
+            <img src={imgLogo} width={185} height={51} alt={"logo"} />
+          </div>
+        </div>
+        <div className="title">
+          Tue Sep 09, 2022
+        </div>
+        <div className="right">
+          <div className="arrow">
+            <div className="left">
+              <svg width="11" height="13" viewBox="0 0 11 13" fill="none">
+                <use href="#arrow-left"/>
+              </svg>
+            </div>
+            <div className="right">
+              <svg width="11" height="13" viewBox="0 0 11 13" fill="none">
+                <use href="#arrow-right"/>
+              </svg>
+            </div>
+          </div>
+          <div className="calendar">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <use href="#calendar-icon"/>
+            </svg>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
