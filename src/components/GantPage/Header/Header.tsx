feb816c7d368565e686d2d23ca3629e54d58c52b
Milyutin Sergey <mcm@mail.ru>
2022-10-15 00:08:51 +0300

    feat(head): динамическое формирование даты в краткой форме

--- src/components/GantPage/Header/Header.tsx
+++ src/components/GantPage/Header/Header.tsx
@@ -3,8 +3,17 @@ import "../../../../source/img/svgIcons/arrow-left.svg";
 import "../../../../source/img/svgIcons/arrow-right.svg";
 import "../../../../source/img/svgIcons/calendar-icon.svg";
 import imgLogo from "../../../../source/img/logo/logo.svg";
+import { useContext, useEffect, useState } from "react";
+import { GrantLoaderContext } from "../GantDataLoader/GantDataLoader";
 
 export default function Header(): JSX.Element {
+  const [date, setDate]: [st: Date | null, set: (st: Date | null) => void] = useState<Date | null>(null);
+  const gantLoaderContext = useContext(GrantLoaderContext);
+
+  useEffect((): void => {
+    setDate(gantLoaderContext.date);
+  }, [gantLoaderContext.date]);
+
   return (
     <header className="header">
       <nav className="nav container">
@@ -14,7 +23,12 @@ export default function Header(): JSX.Element {
           </div>
         </div>
         <div className="title">
-          Tue Sep 09, 2022
+          {date && `${
+            new Intl.DateTimeFormat("en-US", {weekday: "short"}).format(date)
+          } ${new Intl.DateTimeFormat("en-US", {month: "short"}).format(date)
+          } ${new Intl.DateTimeFormat("en-US", {day: "2-digit"}).format(date)
+          }, ${date.getFullYear()}`}
+          {/*Tue Sep 09, 2022*/}
         </div>
         <div className="right">
           <div className="arrow">
