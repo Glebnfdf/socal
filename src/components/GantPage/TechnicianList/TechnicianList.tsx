ae607e31ab5ea4cc3e70fce5460323298177499b
Milyutin Sergey <mcm@mail.ru>
2022-10-15 10:13:04 +0300

    feat(tech): у нас с сервера не приходит аватарка, поэтому пока временное решение ввиде постоянной ссылки на картинку

--- src/components/GantPage/TechnicianList/TechnicianList.tsx
+++ src/components/GantPage/TechnicianList/TechnicianList.tsx
@@ -73,7 +73,8 @@ export default function TechnicianList(): JSX.Element {
                       </div>
                       <div className="person">
                         <img
-                          src={technician.avatar ? technician.avatar : "https://i.ibb.co/C1ZFCsr/person-1.png"}
+                          // src={technician.avatar ? technician.avatar : "https://i.ibb.co/C1ZFCsr/person-1.png"}
+                          src={"https://i.ibb.co/C1ZFCsr/person-1.png"}
                           alt="#"
                         />
                       </div>
