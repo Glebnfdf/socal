d64f6e9bd5adff5b7bd1ecf4466cf2efa51744e3
kotix848 <katocik@protonmail.com>
2022-10-16 23:32:02 +0300

    #19 complited

--- source/js/index.js
+++ source/js/index.js
@@ -3,11 +3,17 @@ import './script';
 import './main';
 
 const scrollbar = new Scrollbar();
+const scrollbarTwo = new Scrollbar();
 document.addEventListener('DOMContentLoaded', initMain);
 
 function initMain() {
   const scrollContainer = document.querySelector('.scroll-cont');
   if (scrollContainer) {
     scrollbar.init(scrollContainer);
+  }
+
+  const scrollContainerTwo = document.querySelector('.scroll-cont-bottom');
+  if (scrollContainerTwo) {
+    scrollbarTwo.init(scrollContainerTwo);
   }
 }
