baf38a24eb6dce088fded91547b5caaec97c72b0
Milyutin Sergey <mcm@mail.ru>
2022-10-24 21:14:28 +0300

    feat(errP): функционал закрытия popUp с простой ошибкой

--- src/components/GantPage/PopUpList/SimpleErrorPopUp/SimpleErrorPopUp.tsx
+++ src/components/GantPage/PopUpList/SimpleErrorPopUp/SimpleErrorPopUp.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 import "../../../../../source/img/svgIcons/error-icon.svg";
 import "../../../../../source/img/svgIcons/close-icon.svg";
+import { useContext, useEffect } from "react";
+import { PopUpName } from "../PopUpList";
+import { iPopUpContext, PopUpContext } from "../../../PopUpContext/PopUpContext";
 
 export default function SimpleErrorPopUp(): JSX.Element {
+  const popUpContext: iPopUpContext = useContext(PopUpContext);
+
+  useEffect((): () => void => {
+    const pageClickHandler = (event: MouseEvent): void => {
+      if ((event.target as HTMLElement).closest(".popup-error") === null) {
+        popUpContext.setData(PopUpName.none, null)
+      }
+    }
+    document.addEventListener("click", pageClickHandler);
+    return (): void => {
+      document.removeEventListener("click", pageClickHandler);
+    };
+  }, []);
+
   return (
     <div className="popup-error">
-      <div className="close">
+      <div className="close" onClick={(): void => {popUpContext.setData(PopUpName.none, null)}}>
         <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
           <use href="#close-icon"/>
         </svg>
@@ -19,7 +36,7 @@ export default function SimpleErrorPopUp(): JSX.Element {
       <div className="text">
         You should change time slot in Bitrix24 if you doesn't set a technician
       </div>
-      <div className="btn-find">
+      <div className="btn-find" onClick={(): void => {popUpContext.setData(PopUpName.none, null)}}>
         Close
       </div>
     </div>
