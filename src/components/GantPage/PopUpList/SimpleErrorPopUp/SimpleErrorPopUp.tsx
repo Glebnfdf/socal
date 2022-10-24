8ecfb1db7a71c8cf90b2ef428effc85156868c32
Milyutin Sergey <mcm@mail.ru>
2022-10-24 21:09:48 +0300

    feat(errP): интеграция вёрстки для простого popUp с ошибкой

--- src/components/GantPage/PopUpList/SimpleErrorPopUp/SimpleErrorPopUp.tsx
+++ src/components/GantPage/PopUpList/SimpleErrorPopUp/SimpleErrorPopUp.tsx
@@ -1,7 +1,27 @@
 import * as React from "react";
+import "../../../../../source/img/svgIcons/error-icon.svg";
+import "../../../../../source/img/svgIcons/close-icon.svg";
 
 export default function SimpleErrorPopUp(): JSX.Element {
   return (
-    <>You should change time slot in Bitrix24 if you doesn't set a technician</>
+    <div className="popup-error">
+      <div className="close">
+        <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
+          <use href="#close-icon"/>
+        </svg>
+      </div>
+      <div className="top">
+        <svg width="37" height="38" viewBox="0 0 37 38" fill="none">
+          <use href="#error-icon"/>
+        </svg>
+        <p className="title">Error!</p>
+      </div>
+      <div className="text">
+        You should change time slot in Bitrix24 if you doesn't set a technician
+      </div>
+      <div className="btn-find">
+        Close
+      </div>
+    </div>
   );
 }
