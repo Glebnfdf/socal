7d43f7f08eb290bff1fc99e4e8f9b68488cd4d56
Milyutin Sergey <mcm@mail.ru>
2022-10-24 21:38:11 +0300

    feat(techP): интеграция вёрстки

--- src/components/GantPage/PopUpList/AddTechPopUp.tsx
+++ src/components/GantPage/PopUpList/AddTechPopUp.tsx
@@ -1,7 +1,145 @@
 import * as React from "react";
+import "../../../../source/img/svgIcons/close-icon.svg";
+import "../../../../source/img/svgIcons/union.svg";
 
 export default function AddTechPopUp(): JSX.Element {
   return (
-    <>Hellow world!</>
+    <div className="popup-search">
+      <div className="close">
+        <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
+          <use href="#close-icon"/>
+        </svg>
+      </div>
+      <div className="title">
+        Add technicial
+      </div>
+      <div className="search-form">
+        <i className="fa fa-address-book-o" aria-hidden="true" id="union">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <use href="#union"/>
+          </svg>
+        </i>
+        <input className="search" placeholder="Search" id="search-input" type="text"/>
+      </div>
+      <main className="main-container">
+        <div className="scroll-cont">
+          <div className="scroll-content-wrapper scroll-content-wrapper-popup-add">
+            <ul className="list">
+              <li className="item">
+                <div className="left">
+                  <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                    <div className="names">
+                      <div className="title-technical">
+                        Technician
+                      </div>
+                      <div className="name">
+                        Matthew David McCon...
+                      </div>
+                    </div>
+                </div>
+                <div className="right">
+                  <div className="add-btn">
+                    Add
+                  </div>
+                </div>
+              </li>
+              <li className="item">
+                <div className="left">
+                  <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                    <div className="names">
+                      <div className="title-technical">
+                        Technician
+                      </div>
+                      <div className="name">
+                        Matthew David McCon...
+                      </div>
+                    </div>
+                </div>
+                <div className="right">
+                  <div className="add-btn">
+                    Add
+                  </div>
+                </div>
+              </li>
+              <li className="item">
+                <div className="left">
+                  <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                    <div className="names">
+                      <div className="title-technical">
+                        Technician
+                      </div>
+                      <div className="name">
+                        Matthew David McCon...
+                      </div>
+                    </div>
+                </div>
+                <div className="right">
+                  <div className="add-btn">
+                    Add
+                  </div>
+                </div>
+              </li>
+              <li className="item">
+                <div className="left">
+                  <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                    <div className="names">
+                      <div className="title-technical">
+                        Technician
+                      </div>
+                      <div className="name">
+                        Matthew David McCon...
+                      </div>
+                    </div>
+                </div>
+                <div className="right">
+                  <div className="add-btn">
+                    Add
+                  </div>
+                </div>
+              </li>
+              <li className="item">
+                <div className="left">
+                  <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                    <div className="names">
+                      <div className="title-technical">
+                        Technician
+                      </div>
+                      <div className="name">
+                        Matthew David McCon...
+                      </div>
+                    </div>
+                </div>
+                <div className="right">
+                  <div className="add-btn">
+                    Add
+                  </div>
+                </div>
+              </li>
+              <li className="item">
+                <div className="left">
+                  <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                    <div className="names">
+                      <div className="title-technical">
+                        Technician
+                      </div>
+                      <div className="name">
+                        Matthew David McCon...
+                      </div>
+                    </div>
+                </div>
+                <div className="right">
+                  <div className="add-btn">
+                    Add
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="scroll-vtrack">
+            <div className="scroll-thumb"></div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
