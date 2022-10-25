ac931cc66a7bdfcf19e7a3cecc5e01dba666be97
Milyutin Sergey <mcm@mail.ru>
2022-10-25 23:11:56 +0300

    feat(map): у каждого маркера показываем bubble с указанием времени заявки

--- src/components/GantPage/GMap/Marker/Marker.tsx
+++ src/components/GantPage/GMap/Marker/Marker.tsx
@@ -9,6 +9,7 @@ interface iProps {
 
 export default function Marker({order, mapRef}: iProps): JSX.Element {
   const markerRef: React.MutableRefObject<google.maps.Marker | null> = useRef<google.maps.Marker | null>(null);
+  const infoWindow: React.MutableRefObject<google.maps.InfoWindow | null> = useRef<google.maps.InfoWindow | null>(null);
 
   useEffect((): () => void => {
     if (order && mapRef) {
@@ -19,10 +20,17 @@ export default function Marker({order, mapRef}: iProps): JSX.Element {
           Number(order.coords.split(";")[1])
         )
       });
+      infoWindow.current = new google.maps.InfoWindow({
+        content: order.time_slot_to
+      });
+      infoWindow.current.open(mapRef, markerRef.current);
     }
     return (): void => {
       if (markerRef.current) {
         markerRef.current.setMap(null);
+      }
+      if (infoWindow.current) {
+        infoWindow.current.close();
       }
     };
   }, []);
