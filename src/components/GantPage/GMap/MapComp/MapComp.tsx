7da5a8c062d3e353083dd6bfa8d8b0a0523ac582
Milyutin Sergey <mcm@mail.ru>
2022-10-25 20:26:58 +0300

    feat(map): формируем динамически у карты набор маркеров

--- src/components/GantPage/GMap/MapComp/MapComp.tsx
+++ src/components/GantPage/GMap/MapComp/MapComp.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { iMapContext, MapContext } from "../../MapProvider/MapProvider";
+import Marker from "../Marker/Marker";
+import { iOrder, iOrderListContext, OrderListContext } from "../../OrderListModel/OrderListModel";
 
 export default function MapComp(): JSX.Element {
   const div4MapRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
@@ -7,6 +10,10 @@ export default function MapComp(): JSX.Element {
   const mapCenter: google.maps.LatLng =
     new google.maps.LatLng(36.65342732855857, -119.89621977146605);
   const defaultZoom: number = 7;
+  const [orderId, setOrderId]: [st: number | null, set: (st: number | null) => void] = useState<number | null>(null);
+  const mapContext: iMapContext = useContext<iMapContext>(MapContext);
+  const orderListContext: iOrderListContext = useContext<iOrderListContext>(OrderListContext);
+  const orderList4Tech: React.MutableRefObject<iOrder[] | null> = useRef<iOrder[] | null>(null);
 
   useEffect((): void => {
     if (div4MapRef.current) {
@@ -17,7 +24,23 @@ export default function MapComp(): JSX.Element {
     }
   }, [div4MapRef.current]);
 
+  useEffect((): void => {
+    setOrderId(mapContext.getMapContextData().orderId);
+    const techId: number | null = mapContext.getMapContextData().techId;
+    if (techId) {
+      orderList4Tech.current = orderListContext.getOrdersByTechId(techId);
+    }
+  }, [mapContext]);
+
   return (
-    <div id={"map"} ref={div4MapRef}></div>
+    <>
+      <div id={"map"} ref={div4MapRef}></div>
+      {orderId && <Marker/>}
+      {orderList4Tech.current && orderList4Tech.current.map((order: iOrder): JSX.Element => {
+        return (
+          <Marker/>
+        )
+      })}
+    </>
   );
 }
---
+++ src/components/GantPage/GMap/Marker/Marker.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function Marker(): JSX.Element {
+  return (
+    <>Hello world</>
+  );
+}
