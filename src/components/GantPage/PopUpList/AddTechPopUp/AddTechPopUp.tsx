99ced7b181c687f9f65105772f086b909a18521a
Milyutin Sergey <mcm@mail.ru>
2022-10-31 23:16:10 +0300

    feat(techP): добавлена анимация появления popup

--- src/components/GantPage/PopUpList/AddTechPopUp/AddTechPopUp.tsx
+++ src/components/GantPage/PopUpList/AddTechPopUp/AddTechPopUp.tsx
@@ -7,6 +7,7 @@ import { iTechListContext, iTechnician, TechListContext } from "../../Technician
 import { iPopUpContext, PopUpContext } from "../../../PopUpContext/PopUpContext";
 import { PopUpName } from "../PopUpList";
 import { iOrderPopUpInData, OrderPopUpType } from "../OrderPopUp/OrderPopUp";
+import { CSSTransition } from "react-transition-group";
 
 interface iProps {
   incomingData: iAddTechInData | null
@@ -45,102 +46,104 @@ export default function AddTechPopUp({incomingData}: iProps): JSX.Element {
   }, []);
 
   return (
-    <div className="popup-search">
-      <div className="close" onClick={(): void => {
-        if (!incomingData) {
-          return;
-        }
-        const transmittedDate: iOrderPopUpInData = {
-          type: OrderPopUpType.Big,
-          orderId: incomingData.orderId,
-          orderElm: null,
-          container: null,
-          mainTechId: incomingData.mainTechId,
-          secondTechId: incomingData.secondTechId
-        }
-        popUpContext.setData(PopUpName.orderPopUp, transmittedDate);
-      }}>
-        <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
-          <use href="#close-icon"/>
-        </svg>
-      </div>
-      <div className="title">
-        Add technician
-      </div>
-      <div className="search-form">
-        <i className="fa fa-address-book-o" aria-hidden="true" id="union">
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-            <use href="#union"/>
+    <CSSTransition in={true} timeout={300} classNames={"popup-transition"} appear unmountOnExit={true}>
+      <div className="popup-search">
+        <div className="close" onClick={(): void => {
+          if (!incomingData) {
+            return;
+          }
+          const transmittedDate: iOrderPopUpInData = {
+            type: OrderPopUpType.Big,
+            orderId: incomingData.orderId,
+            orderElm: null,
+            container: null,
+            mainTechId: incomingData.mainTechId,
+            secondTechId: incomingData.secondTechId
+          }
+          popUpContext.setData(PopUpName.orderPopUp, transmittedDate);
+        }}>
+          <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
+            <use href="#close-icon"/>
           </svg>
-        </i>
-        <input
-          className="search"
-          placeholder="Search"
-          id="search-input"
-          type="text"
-          value={searchText}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {setSearchText(event.target.value)}}
-        />
-      </div>
-      <main className="main-container">
-        <div id={"add-tech-scroll-cont"} className="scroll-cont">
-          <div className="scroll-content-wrapper scroll-content-wrapper-popup-add">
-            <ul className="list">
-              {techListContext.techList.map((technician: iTechnician): JSX.Element | null => {
-                let isShowTechInList: boolean = true;
-                if (incomingData) {
-                  if (incomingData.operationType === AddTechOperationType.AddMainTech &&
-                      incomingData.secondTechId === technician.id) {
-                    isShowTechInList = false;
-                  }
-                  if (incomingData.operationType === AddTechOperationType.AddSecondTech &&
-                    incomingData.mainTechId === technician.id) {
-                    isShowTechInList = false;
-                  }
-                }
-                if (!technician.name.toLowerCase().includes(searchText.toLowerCase())) {
-                  isShowTechInList = false;
-                }
-                if (!isShowTechInList) {
-                  return null;
-                }
-                return (
-                  <li className="item" key={technician.id}>
-                    <div className="left">
-                      <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
-                      <div className="names">
-                        <div className="title-technical">Technician</div>
-                        <div className="name">{technician.name}</div>
-                      </div>
-                    </div>
-                    <div className="right">
-                      <div className="add-btn" onClick={(): void => {
-                        if (!incomingData) {
-                          return;
-                        }
-                        const transmittedDate: iOrderPopUpInData = {
-                          type: OrderPopUpType.Big,
-                          orderId: incomingData.orderId,
-                          orderElm: null,
-                          container: null,
-                          mainTechId: incomingData.operationType === AddTechOperationType.AddMainTech
-                            ? technician.id : incomingData.mainTechId,
-                          secondTechId: incomingData.operationType === AddTechOperationType.AddSecondTech
-                            ? technician.id : incomingData.secondTechId
-                        }
-                        popUpContext.setData(PopUpName.orderPopUp, transmittedDate);
-                      }}>Add</div>
-                    </div>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="scroll-vtrack">
-            <div className="scroll-thumb"></div>
-          </div>
         </div>
-      </main>
-    </div>
+        <div className="title">
+          Add technician
+        </div>
+        <div className="search-form">
+          <i className="fa fa-address-book-o" aria-hidden="true" id="union">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <use href="#union"/>
+            </svg>
+          </i>
+          <input
+            className="search"
+            placeholder="Search"
+            id="search-input"
+            type="text"
+            value={searchText}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {setSearchText(event.target.value)}}
+          />
+        </div>
+        <main className="main-container">
+          <div id={"add-tech-scroll-cont"} className="scroll-cont">
+            <div className="scroll-content-wrapper scroll-content-wrapper-popup-add">
+              <ul className="list">
+                {techListContext.techList.map((technician: iTechnician): JSX.Element | null => {
+                  let isShowTechInList: boolean = true;
+                  if (incomingData) {
+                    if (incomingData.operationType === AddTechOperationType.AddMainTech &&
+                        incomingData.secondTechId === technician.id) {
+                      isShowTechInList = false;
+                    }
+                    if (incomingData.operationType === AddTechOperationType.AddSecondTech &&
+                      incomingData.mainTechId === technician.id) {
+                      isShowTechInList = false;
+                    }
+                  }
+                  if (!technician.name.toLowerCase().includes(searchText.toLowerCase())) {
+                    isShowTechInList = false;
+                  }
+                  if (!isShowTechInList) {
+                    return null;
+                  }
+                  return (
+                    <li className="item" key={technician.id}>
+                      <div className="left">
+                        <img className="person-img" src="https://i.ibb.co/C1ZFCsr/person-1.png" alt="#"/>
+                        <div className="names">
+                          <div className="title-technical">Technician</div>
+                          <div className="name">{technician.name}</div>
+                        </div>
+                      </div>
+                      <div className="right">
+                        <div className="add-btn" onClick={(): void => {
+                          if (!incomingData) {
+                            return;
+                          }
+                          const transmittedDate: iOrderPopUpInData = {
+                            type: OrderPopUpType.Big,
+                            orderId: incomingData.orderId,
+                            orderElm: null,
+                            container: null,
+                            mainTechId: incomingData.operationType === AddTechOperationType.AddMainTech
+                              ? technician.id : incomingData.mainTechId,
+                            secondTechId: incomingData.operationType === AddTechOperationType.AddSecondTech
+                              ? technician.id : incomingData.secondTechId
+                          }
+                          popUpContext.setData(PopUpName.orderPopUp, transmittedDate);
+                        }}>Add</div>
+                      </div>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+            <div className="scroll-vtrack">
+              <div className="scroll-thumb"></div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </CSSTransition>
   );
 }
