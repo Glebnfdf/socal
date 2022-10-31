f35b4f1b37ab3b10099380f84a424a98d28ba270
Milyutin Sergey <mcm@mail.ru>
2022-10-31 23:21:05 +0300

    feat(errP): добавлена анимация появления popup

--- src/components/GantPage/PopUpList/SimpleErrorPopUp/SimpleErrorPopUp.tsx
+++ src/components/GantPage/PopUpList/SimpleErrorPopUp/SimpleErrorPopUp.tsx
@@ -4,29 +4,32 @@ import "../../../../../source/img/svgIcons/close-icon.svg";
 import { useContext } from "react";
 import { PopUpName } from "../PopUpList";
 import { iPopUpContext, PopUpContext } from "../../../PopUpContext/PopUpContext";
+import { CSSTransition } from "react-transition-group";
 
 export default function SimpleErrorPopUp(): JSX.Element {
   const popUpContext: iPopUpContext = useContext(PopUpContext);
 
   return (
-    <div className="popup-error">
-      <div className="close" onClick={(): void => {popUpContext.setData(PopUpName.none, null)}}>
-        <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
-          <use href="#close-icon"/>
-        </svg>
+    <CSSTransition in={true} timeout={300} classNames={"popup-transition"} appear unmountOnExit={true}>
+      <div className="popup-error">
+        <div className="close" onClick={(): void => {popUpContext.setData(PopUpName.none, null)}}>
+          <svg width="11" height="11" viewBox="0 0 11 11" fill="none">
+            <use href="#close-icon"/>
+          </svg>
+        </div>
+        <div className="top">
+          <svg width="37" height="38" viewBox="0 0 37 38" fill="none">
+            <use href="#error-icon"/>
+          </svg>
+          <p className="title">Error!</p>
+        </div>
+        <div className="text">
+          You should change time slot in Bitrix24 if you doesn't set a technician
+        </div>
+        <div className="btn-find" onClick={(): void => {popUpContext.setData(PopUpName.none, null)}}>
+          Close
+        </div>
       </div>
-      <div className="top">
-        <svg width="37" height="38" viewBox="0 0 37 38" fill="none">
-          <use href="#error-icon"/>
-        </svg>
-        <p className="title">Error!</p>
-      </div>
-      <div className="text">
-        You should change time slot in Bitrix24 if you doesn't set a technician
-      </div>
-      <div className="btn-find" onClick={(): void => {popUpContext.setData(PopUpName.none, null)}}>
-        Close
-      </div>
-    </div>
+    </CSSTransition>
   );
 }
