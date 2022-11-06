ed93d12ef988a3dcfb2e595bb21b079ca8578227
Milyutin Sergey <mcm@mail.ru>
2022-11-06 15:34:08 +0300

    fix(tech): ошибка в условии пересекается ли время заявки с не рабочими часами техника

--- src/utils/notCrossNonWorkTimes.ts
+++ src/utils/notCrossNonWorkTimes.ts
@@ -9,7 +9,8 @@ export default function notCrossNonWorkTimes(
     const techTmeSlotStart: Date = new Date(techNonWorkingTimes[i].start);
     const techTmeSlotEnd: Date = new Date(techNonWorkingTimes[i].finish);
     if (orderBeginTime.getTime() > techTmeSlotStart.getTime() && orderBeginTime.getTime() < techTmeSlotEnd.getTime() ||
-        orderEndTime.getTime() > techTmeSlotStart.getTime() && orderEndTime.getTime() < techTmeSlotEnd.getTime()
+        orderEndTime.getTime() > techTmeSlotStart.getTime() && orderEndTime.getTime() < techTmeSlotEnd.getTime() ||
+        orderBeginTime.getTime() <= techTmeSlotStart.getTime() && orderEndTime.getTime() >= techTmeSlotEnd.getTime()
     ) {
       return false;
     }
